d8394f6100efa057757427bef8faeead288da8cf
Michael Melkonian <mikemel20@gmail.com>
2021-07-25 12:54:14 -0700

    Initial Maven Dependency Commit (JUnit) -Michael Melkonian

---
+++ assets/maven-assignment/src/test/java/com/codebind/AppMikeTest.java
@@ -0,0 +1,14 @@
+package com.codebind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppMikeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
